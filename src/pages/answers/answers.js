183d21403816ef2fa60c192806c076e77f3cf42f
levelopers <38830527+levelopers@users.noreply.github.com>
2018-12-18 00:02:54 -0500

    12/18

--- src/pages/answers/answers.js
+++ src/pages/answers/answers.js
@@ -13,22 +13,31 @@ class Answers extends Component {
     }
   }
   componentDidMount() {
-    
-    if (!!this.props.answers) {
-      return this.props.getAnswerWithQuestionId(this.state.question_id)
+    console.log(this.props.answers);
+    //if refresh
+    if (this.props.answers.length<1) {
+      this.props.getAnswerWithQuestionId(this.state.question_id)
     }
-    const local_answers = this.props.answers.filter(
-      ans => {
-        return ans.question_id === this.state.question_id
-      }
-    )
-    this.setState({
-      local_answers: local_answers[0].answers
-    })
+     
+      //from question page
+      else{
+      const local_answers = this.props.answers.filter(
+        ans => {
+          return ans.question_id === this.state.question_id
+        }
+      )
+      this.setState({
+        local_answers: local_answers[0].answers
+      })
+    }
+
+
   }
   componentDidUpdate() {
     //state null wait for redux updating props
-    if (!this.state.local_answers&&this.props.answers[0]) {
+    console.log(this.props.user);
+
+    if (!this.state.local_answers && this.props.answers[0]) {
       //request user name & avatar_url
       const user_ids = []
       for (let ans of this.props.answers[0]) {
@@ -40,26 +49,28 @@ class Answers extends Component {
         local_answers: this.props.answers[0]
       })
     }
-    if(!this.props.user&&this.props.token){
+    
+
+    if (Object.keys(this.props.user).length<1) {
       this.props.getProfile(this.props.token.user_id)
     }
-    
   }
+
 
 
   render() {
 
     return (
       <div className={styles.page}>
-      <div>
-        <Header img={this.props.user&&this.props.user.avatar_url}/>
-      </div>
+        <div>
+          <Header img={this.props.user && this.props.user.avatar_url} />
+        </div>
         {this.state.local_answers && this.state.local_answers.map(ans =>
           this.props.users.map(user =>
             ans.user_id === user.id &&
             <div key={`${ans.id}-${user.id}`}>
               {user.name}
-              {new Date(user.updated_at).toString().split(' ').slice(1,4).join('-')}
+              {new Date(user.updated_at).toString().split(' ').slice(1, 4).join('-')}
               {ans.content}
             </div>
           )
@@ -71,10 +82,10 @@ class Answers extends Component {
 }
 
 const mapStatetoProps = (state) => ({
+  token: state.token.token,
   answers: state.answers.answers,
   user: state.profile.user,
   users: state.profile.users,
-  token:state.token.token
 })
 
 export default connect(mapStatetoProps, { getAnswerWithQuestionId, getProfile, getProfiles })(Answers)
