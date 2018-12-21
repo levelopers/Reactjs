fcde280e79e640dad6e5a7809016a6d29609d2b1
levelopers <youcancallmeallen@gmail.com>
2018-12-21 01:15:25 -0500

    profile bugs and styles fixed, header click portr,

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,18 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { withRouter } from 'react-router'
 import './header.css'
 
-const Header = (props) => {
-  let img_src = props.input_file_path || props.img || '/avatar_default.jpg'
-  return (
-    <div className="header">
-      <div className="logo">
-        <h3>BIG FISH</h3>
+// const Header = (props) => {
+//   console.log(this.props);
+
+
+//   return (
+//     <div className="header">
+//       <div className="logo">
+//         <h3>BIG FISH</h3>
+//       </div>
+//       <div className="portrait" onClick={e=>handleClick(e,props)}>
+//         <img src={img_src} alt="portrait" />
+//       </div>
+//     </div>
+//   )
+// }
+
+
+
+class Header extends Component {
+  constructor(props) {
+    super(props)
+  }
+  handleClick = () => {
+    this.props.history.push('/profile')
+  }
+  render() {
+    let img_src = this.props.input_file_path || this.props.img || '/avatar_default.jpg'
+    return (
+      <div className="header">
+        <div className="logo">
+          <h3>BIG FISH</h3>
+        </div>
+        <div className="portrait" onClick={this.handleClick}>
+          <img src={img_src} alt="portrait" />
+        </div>
       </div>
-      <div className="portrait">
-        <img src={img_src} alt="portrait" />
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
-export default Header
+
+export default withRouter(Header)
